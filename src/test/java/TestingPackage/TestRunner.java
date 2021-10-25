fb3e1f12b7aebab38a846bc53aa025da5cb42bdb
Heba-Raafat <heba.raafat95@gmail.com>
2021-10-25 13:13:47 +0200

    Adapting to intelJ structure

---
+++ src/test/java/TestingPackage/TestRunner.java
@@ -0,0 +1,22 @@
+package test.java.TestingPackage;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+        plugin ={"pretty",
+        		"html:target/colourLovers_Status_Report.html",
+                "json:target/colourLovers_Status_Report.json",
+                },
+        //"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
+        features = "src/test/resources/features",
+        glue={"test.java.TestingPackage"},
+        monochrome = true
+
+        )
+
+public class TestRunner extends AbstractTestNGCucumberTests{
+	
+
+}
+
